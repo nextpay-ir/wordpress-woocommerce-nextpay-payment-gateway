a887452eb105bd64accfb680e36851a5f2f68ecd
Farzad Latifnia <register.site0@gmail.com>
2016-08-13 18:07:51 +0430

    comment for plugin

--- index.php
+++ index.php
@@ -8,6 +8,15 @@
  * Website:
  * Email:
  * @copyright 2016
+ * @package NextPay_Gateway
+ * @version 1.0
  */
-
-include_once("class-gateway-nextpay.php");
+/*
+  Plugin Name: افزونه پرداخت امن نکست پی برای ووکامرس
+  Plugin URI: http://www.nextpay.ir
+  Description: این افزونه درگاه پرداخت امن در نکست پی برای فروشگاه ساز ووکامرس میباشد
+  Author: @FreezeMan
+  Version: 1.0
+  Author URI: http://www.nextpay.ir
+*/
+include_once("class-wc-gateway-nextpay.php");
